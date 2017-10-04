25ff760f8d21179f0eb617db745119b8cfe3c75a
Jen Yu <jyu7@stuy.edu>
2017-10-04 16:57:09 -0400

    NOT WORKING AH

--- demo_string.c
+++ demo_string.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <str_fxn.h>
+#include "str_fxn.h"
 
 int main()
 {
-  //printf("this better print\n");
   char * str1 = "hello";
   char * str2;
-  //printf("part 1\n");
+  printf("part 1\n");
   mystrncpy(str2, str1, 4);
 }
 
--- str_fxn.h
+++ str_fxn.h
@@ -1,3 +1,9 @@
+int mystrlen( char *str);
+char * mystrncpy(char *dest, char *source, int size);
+char * mystrcat(char *dest, char *souce);
+int mystrcmp(char *s1, char *s2);
+char * mystrchr(char *s, char c);
+
 //Returns the length of a null terminated string
 int mystrlen(char *str)
 {
@@ -15,8 +21,13 @@ char * mystrncpy(char *dest, char *source, int size)
   int i;
   for (i = 0; i < size && source[i] != '\0'; i++)
     {
+      printf("What tf is happening");
+      printf("%s", dest);
+      printf("%s", source);
+      printf("currently at value %d \n", dest[i]);
       dest[i] = source[i];
+      //printf("did we make it? \n");
     }
-  dest[i] '\0';
+  dest[i] = '\0';
   return dest;
 }
